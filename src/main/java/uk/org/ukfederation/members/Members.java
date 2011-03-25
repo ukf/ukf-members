717441ab50f9cb2de079490d1f4cfd0d05d26469
Ian Young <ian@iay.org.uk>
2011-03-25 16:28:07 +0000

    Initial checkin of ukf-members.

---
+++ src/main/java/uk/org/ukfederation/members/Members.java
@@ -0,0 +1,123 @@
+/*
+ * Simplified access to the UK federation members.xml database.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.ukfederation.members;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.bind.JAXB;
+import javax.xml.transform.dom.DOMSource;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import uk.org.ukfederation.members.jaxb.MemberElement;
+import uk.org.ukfederation.members.jaxb.MembersElement;
+import uk.org.ukfederation.members.jaxb.NonMemberElement;
+
+/*
+ * Java bean representing the whole of the members.xml file.
+ * 
+ * This class is responsible for parsing the file into a basic object graph using JAXB, and
+ * provides a facade simplifying access to much of the functionality required by client
+ * applications.
+ * 
+ * Author: Ian A. Young, ian@iay.org.uk
+ */
+public class Members {
+	
+	/**
+	 * The root of the underlying JAXB object graph.
+	 */
+	private final MembersElement membersElement;
+
+	/**
+	 * Set of all the owner names within the members document.
+	 */
+	private final Set<String> ownerNames = new HashSet<String>();
+	
+	/**
+	 * Internal constructor, called by all public constructors.
+	 * 
+	 * @param m The {@link MembersElement} on which to base the new bean.
+	 */
+	private Members(MembersElement m) {
+		this.membersElement = m;
+
+		/*
+		 * Collect names of members.
+		 */
+		for (MemberElement member: membersElement.getMember()) {
+			ownerNames.add(member.getName());
+		}
+		
+		/*
+		 * Collect names of non-members.
+		 */
+		for (NonMemberElement nonMember: membersElement.getNonMember()) {
+			ownerNames.add(nonMember.getName());
+		}
+	}
+	
+	/**
+	 * Constructs a {@link Members} object from an XML document represented as a stream.
+	 * 
+	 * @param stream
+	 */
+	public Members(InputStream stream) {
+		this(JAXB.unmarshal(stream, MembersElement.class));
+	}
+	
+	/**
+	 * Returns a {@link DOMSource} derived from the provided DOM {@link Node}.
+	 * If the node is a {@link Document}, dig down to its document element.  This
+	 * sometimes helps JAXB when called from within Xalan.
+	 * 
+	 * @param node DOM node to start from.
+	 * @return An appropriate {@link DOMSource}.
+	 */
+	private static DOMSource makeDOMSource(Node node) {
+		if (node == null)
+			throw new NullPointerException("provided DOM node is null");
+		
+		if (node instanceof Document) {
+			Document d = (Document)node;
+			node = d.getDocumentElement();
+		}
+		return new DOMSource(node);
+	}
+	
+	/**
+	 * Constructs a {@link Members} object from an XML document represented as a DOM
+	 * {@link Node} object.
+	 */
+	public Members(Node doc) {
+		this(JAXB.unmarshal(makeDOMSource(doc), MembersElement.class));
+	}
+	
+	/**
+	 * Checks for a legitimate entity owner name: either the name of a federation member
+	 * or the name of a known non-member entity owner.
+	 * 
+	 * @param s	Name to check.
+	 * @return {@code true} if and only if {@code s} contains a legitimate entity owner name.
+	 */
+	public boolean isOwnerName(String s) {
+		return ownerNames.contains(s);
+	}
+	
+}
---
+++ src/test/java/uk/org/ukfederation/members/MembersTest.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.ukfederation.members;
+
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+
+import org.custommonkey.xmlunit.Diff;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+
+/**
+ * Tests for the {@link Members} class.
+ * 
+ * Makes use of TestNG and XMLUnit.
+ * 
+ * @author iay
+ */
+public class MembersTest {
+	
+	private DocumentBuilderFactory dbFactory;
+	private TransformerFactory tFactory;
+	
+	private InputStream streamResource(String resourceName) {
+		return MembersTest.class.getResourceAsStream("/" + resourceName);
+	}
+	
+	private Members fetchMembers(String resourceName) {
+		return new Members(streamResource(resourceName));
+	}
+	
+	private Document fetchDocument(String resourceName) throws Exception {
+		DocumentBuilder db = dbFactory.newDocumentBuilder();
+		return db.parse(streamResource(resourceName));
+	}
+	
+	@BeforeClass
+	public void setUp() throws Exception{
+		dbFactory = DocumentBuilderFactory.newInstance();
+		dbFactory.setNamespaceAware(true);
+		dbFactory.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
+		tFactory = TransformerFactory.newInstance();
+	}
+	
+	/**
+	 * Tests {@link Members.isOwnerName} with simple {@link String} parameters.
+	 * 
+	 * Also, inter alia, tests the stream constructor.
+	 */
+	@Test
+	public void testIsOwnerNameString() {
+		Members m = fetchMembers("oneOfEach.xml");
+		Assert.assertTrue(m.isOwnerName("Valid Member"));
+		Assert.assertTrue(m.isOwnerName("Valid Non Member"));
+		Assert.assertFalse(m.isOwnerName("Should not be present"));
+	}
+
+	/**
+	 * Tests construction of a {@link Members} object from a DOM document.
+	 */
+	@Test
+	public void testConstructorDocument() throws Exception {
+		Members m = new Members(fetchDocument("oneOfEach.xml"));
+		Assert.assertTrue(m.isOwnerName("Valid Member"));
+		Assert.assertTrue(m.isOwnerName("Valid Non Member"));
+		Assert.assertFalse(m.isOwnerName("Should not be present"));
+	}
+	
+	/**
+	 * Tests the {@link isMemberName} predicate used from within XSLT.
+	 */
+	@Test
+	public void testIsOwnerNameXSLT() throws Exception {
+		Document in = fetchDocument("isMemberNameIn.xml");
+		Document tr = fetchDocument("isMemberName.xsl");
+		Document ok = fetchDocument("isMemberNameOK.xml");
+		Transformer t = tFactory.newTransformer(new DOMSource(tr));
+		DOMResult out = new DOMResult();
+		t.transform(new DOMSource(in), out);
+		Document ou = (Document)(out.getNode());
+		Diff diff = new Diff(ok, ou);
+		Assert.assertTrue(diff.identical(), "diff comparison should have been identical: " + diff);
+	}
+	
+}
